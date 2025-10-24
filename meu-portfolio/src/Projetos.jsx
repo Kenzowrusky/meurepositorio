b7c638f2d6c0b65939a80d23d5de2050100ab25e
Kenzowrusky <kenzo.o.camargo@gmail.com>
2025-10-24 16:13:27 -0300

    reposicionando as imagens em 'public' ao invés de 'assets'

--- meu-portfolio/src/Projetos.jsx
+++ meu-portfolio/src/Projetos.jsx
@@ -7,12 +7,12 @@ function Projetos()
             <h2>Meus Projetos</h2>
             <div className="projetos-grid">
                 <div className = "projeto-card">
-                    <img src="./src/assets/projeto1.png" alt="Projeto 1" />
+                    <img src="./public/projeto1.png" alt="Projeto 1" />
                     <h3>Projeto 1</h3>
                     <p>Meu primeiro projeto foi desenvolvido durante a matéira de SI202, e consiste em um sistema de gerenciamento de agenda de dogsitter, cuidadores de cães, sistema esse responsável por organizar agenda, consumidores, colaboradores e garantindo uma melhora da qualidade para a empresa que oferece esse serviço.</p>
                 </div>
                 <div className = "projeto-card">
-                    <img src="./src/assets/projeto2.png" alt="Projeto 2" />
+                    <img src="./public/projeto2.png" alt="Projeto 2" />
                     <h3>Projeto 2</h3>
                     <p>Meu segundo projeto foi desenvolvido durante a matéria de SI202, e consiste em um programa de manipulação de arquivos, responsável por guardar informações de alunos da faculdade em um banco de dados.</p>
                 </div>
